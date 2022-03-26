5a3b8c513d29477c8318ad44e50be56a806a5af1
Chris Allen Moore <chris@chrisallenmoore.com>
2022-03-26 12:26:26 +0700

    add Tailwind CSS

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,22 +1,5 @@
 <script>
-  let counter = 0;
-  const addToCounter = () => {
-    counter += 1;
-  }
+  import "../../client/main.css";
 </script>
 
-
-<div class="container">
-  <h1>Welcome to Meteor!</h1>
-
-  <button on:click={addToCounter}>Click Me</button>
-  <p>You've pressed the button {counter} times.</p>
-
-  <h2>Learn Meteor!</h2>
-  <ul>
-    <li><a href="https://svelte-tutorial.meteor.com/" target="_blank">Do the Tutorial</a></li>
-    <li><a href="http://guide.meteor.com" target="_blank">Follow the Guide</a></li>
-    <li><a href="https://docs.meteor.com" target="_blank">Read the Docs</a></li>
-    <li><a href="https://forums.meteor.com" target="_blank">Discussions</a></li>
-  </ul>
-</div>
+<h1 class="text-4xl">Testing Tailwind</h1>
